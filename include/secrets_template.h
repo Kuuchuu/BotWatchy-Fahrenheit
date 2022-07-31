db6ce3929a35453babc0ac35a8f651139ae0416a
Blase <Blase.Zinck@gmail.com>
2022-07-31 10:54:45 -0500

    Add files via upload

--- include/secrets_template.h
+++ include/secrets_template.h
@@ -1,6 +1,8 @@
 // copy this file as 'secrets.h', enter your location as LON, LAT
 // and your openweathermap.org API key as OWN_API_KEY
+// Timezone formated as such: America/Chicago
 
 #define LAT ""
 #define LON ""
 #define OWN_API_KEY ""
+#define TIMEZONE "" 
